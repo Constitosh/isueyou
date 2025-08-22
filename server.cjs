fa0a147581710dcd8212331eeb0eab00f4275a77
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-08-22 10:47:00 +0200

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -11,14 +11,14 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-app.use(express.json({ limit: '2mb' }));
-
 const PUBLIC_DIR = path.join(__dirname, "public");
 console.log("Serving static from:", PUBLIC_DIR);
 
+app.use(express.json({ limit: '2mb' }));
 app.use(express.static('PUBLIC_DIR'));
 
 // ------------------------------- Paths & helpers
+const PORT = process.env.PORT || 8080;
 const DATA_DIR = path.join(__dirname, 'data');
 const FILE_TOKENS = path.join(DATA_DIR, 'tokens-lib.json');
 const FILE_SNAPSHOTS = path.join(DATA_DIR, 'snapshots.json');
